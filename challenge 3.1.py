b7b7d3203ba2d3e572320762ebd44491e2fa47b7
autnm1647212205243 <150319059+autnm1647212205243@users.noreply.github.com>
2023-11-09 10:59:29 +0530

    Add files via upload

---
+++ challenge 3.1.py
@@ -0,0 +1,40 @@
+#3.1 Write a function called lin_search that takes a list of product name as input. the function should perform a linear search to find the targer product in the list and return a list of indices of all occurrences of the product if found,or an empty list if the prouct is not found. 
+def linear_search_product(product_list,target_product):
+
+    indices = []
+  
+    for i, product indices
+enumerate(product_list):
+
+   if product ==
+target_product:
+
+     indices.append(i)
+     
+  return indices
+
+
+# Example usage:
+
+products = ["apple","banana",
+"apple","orange","apple"]         
+
+target = "apple"
+
+result = linear_searc
+h_product(products,
+target)         
+
+if result:
+
+   print(f"The product '{target}' was found at indices:{result}")
+
+else
+   print(f"The product'{target}'
+was not found in the list.")
+  
+  
+
+  
+  
+
